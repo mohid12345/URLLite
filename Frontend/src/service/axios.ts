31684df3071460f288433f43a7e262a7d2727766
Mohid Mohan <mohidmohan8482@gmail.com>
2025-03-18 19:48:55 +0530

    chane

--- Frontend/src/service/axios.ts
+++ Frontend/src/service/axios.ts
@@ -2,7 +2,8 @@ import axios, { AxiosInstance } from "axios";
 
 
 const Api:AxiosInstance = axios.create({
-    baseURL:"http://localhost:3000",
+    // baseURL:"http://localhost:3000",
+    baseURL:"http://13.202.106.237",
     withCredentials:true
 });
 
