06a6403594c30683af74bda3f91adf301507e47c
Mohid Mohan <mohidmohan8482@gmail.com>
2025-03-19 02:23:10 +0530

    change

--- Frontend/src/service/axios.ts
+++ Frontend/src/service/axios.ts
@@ -1,13 +1,24 @@
 import axios, { AxiosInstance } from "axios";
 
-
-const Api:AxiosInstance = axios.create({
-    // baseURL:"http://localhost:3000",
-    baseURL:"http://13.202.106.237",
-    withCredentials:true
+const Api: AxiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:3000", 
+    withCredentials: true
 });
 
-export default Api
+export default Api;
+
+
+// import axios, { AxiosInstance } from "axios";
+
+
+// const Api: AxiosInstance = axios.create({
+
+//     baseURL: "http://13.202.106.237",
+//         // baseURL:"http://localhost:3000",
+//     withCredentials: true
+// });
+
+// export default Api
 
 
 
