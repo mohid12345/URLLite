a700669c5814cd22982737a0d13b2af8f5fd5a4e
Mohid Mohan <mohidmohan8482@gmail.com>
2025-03-19 09:26:00 +0530

    change

--- Frontend/src/service/axios.ts
+++ Frontend/src/service/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const Api: AxiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:3000", 
+    baseURL: process.env.REACT_APP_BASE_URL , 
     withCredentials: true
 });
 
